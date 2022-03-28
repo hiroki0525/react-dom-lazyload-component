059da694ebc728486d3b676d538132090e623104
Hiroki Miyaji <hdekm852@gmail.com>
2022-03-29 05:36:19 +0900

    Add scroll list to examples

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,5 +1,29 @@
 import { render } from 'react-dom';
 import LazyLoad from '../src';
+import './index.css';
 
-const App = () => <LazyLoad />;
+const lazyLoadRender = (isVisible: boolean): JSX.Element => {
+  return <>{isVisible ? 'Visible!!' : 'InVisible!!'}</>;
+};
+
+const rootSelectorId = 'scrollList';
+
+const App = () => {
+  return (
+    <section>
+      <h1>Demo</h1>
+      <ul id={rootSelectorId}>
+        {[...Array(10)].map((_, index) => (
+          <LazyLoad
+            key={index}
+            render={lazyLoadRender}
+            rootSelectorId={rootSelectorId}
+            as='li'
+            className='row'
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
 render(<App />, document.getElementById('root'));
