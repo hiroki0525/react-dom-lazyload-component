89053f8bd0a41ba2e9937c3bf2e54c2cae9d2199
Hiroki Miyaji <hdekm852@gmail.com>
2022-03-29 03:06:03 +0900

    Fix lint rule and execute

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    mode: "development",
-    entry: "./src/index.tsx",
-    output: {
-        path: `${__dirname}/dist`,
-        filename: "main.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader"
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    target: ["web", "es5"],
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  target: ['web', 'es5'],
 };
