3cfcde4bb80b1ec20be2c432e3f01cce07ea8e97
Hiroki Miyaji <hdekm852@gmail.com>
2022-03-29 04:18:58 +0900

    Add LazyLoad Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,54 @@
-export default function LazyLoad(): JSX.Element {
-  return <>test</>;
+import { ReactNode, useEffect, useRef, useState } from 'react';
+
+export type LazyLoadProps = {
+  readonly render: (isVisible: boolean) => ReactNode;
+  readonly forceVisible: boolean;
+  readonly visibleCallback?: Function;
+} & IntersectionObserverInit;
+
+export default function LazyLoad({
+  render,
+  forceVisible = false,
+  root,
+  rootMargin,
+  threshold,
+}: LazyLoadProps): ReactNode {
+  const [isVisible, setIsVisible] = useState(forceVisible);
+  const show = () => setIsVisible(true);
+  const ref = useRef();
+
+  useEffect(() => {
+    setIsVisible(forceVisible);
+  }, [forceVisible]);
+
+  useEffect(() => {
+    const el = ref.current;
+    if (el) {
+      const options = {
+        root,
+        rootMargin,
+        threshold,
+      };
+      const checkInViewportAndShow: IntersectionObserverCallback = (
+        entries,
+        observer
+      ) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          show();
+          observer.disconnect();
+        }
+      };
+      const observer = new IntersectionObserver(
+        checkInViewportAndShow,
+        options
+      );
+      observer.observe(el);
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [ref.current]);
+
+  return render(isVisible);
 }
