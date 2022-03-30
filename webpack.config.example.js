6b9979871a2a75d1783822d4dd067cad9e36c1f7
Hiroki Miyaji <hdekm852@gmail.com>
2022-03-30 20:55:39 +0900

    Add webpack config for examples

---
+++ webpack.config.example.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, 'examples/index.html'),
+  filename: './index.html',
+});
+
+module.exports = {
+  entry: path.join(__dirname, 'examples/index.tsx'),
+  output: {
+    path: `${__dirname}/dist/examples`,
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|ico)$/,
+        use: 'url-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  target: ['web', 'es5'],
+  plugins: [htmlWebpackPlugin],
+  devServer: {
+    port: 3001,
+  },
+};
