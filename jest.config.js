0fa8cb75ab24e4b308cd3a02ef3f7298ddef8da5
Hiroki Miyaji <hdekm852@gmail.com>
2022-04-03 19:27:07 +0900

    Restructure src directory

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testEnvironment: 'jsdom',
   testMatch: [
-    '**/__tests__/**/*.+(ts|tsx|js)',
-    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+    '**/*\\.test\\.tsx',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
