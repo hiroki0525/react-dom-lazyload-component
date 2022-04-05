0dee3bfe16d11194a6da358749cc7ba2b9e6f6b4
Hiroki Miyaji <hdekm852@gmail.com>
2022-04-05 18:57:02 +0900

    Add tmp test

--- src/LazyLoad/index.test.tsx
+++ src/LazyLoad/index.test.tsx
@@ -3,7 +3,11 @@ import { cleanup } from '@testing-library/react';
 describe('LazyLoad', () => {
   afterEach(cleanup);
 
-  describe('out of view', () => {});
+  describe('out of view', () => {
+    it('tmp test', () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
 
   describe('in view', () => {});
 });
