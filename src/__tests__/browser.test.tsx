df2d0778df1bec4f922b859a2e6a0e3bcbbef7ac
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-03 08:36:33 +0900

    test: add debug logging

--- src/__tests__/browser.test.tsx
+++ src/__tests__/browser.test.tsx
@@ -4,6 +4,10 @@ describe('Browser', () => {
   jest.setTimeout(10000);
 
   beforeAll(async () => {
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    page.on('pageerror', error => {
+      console.log(error.message);
+    });
     await page.goto('http://localhost:3001', { waitUntil: 'networkidle0' });
   });
 
