ec21db2773c184e750e647492dcf24dcc1bfd88c
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-03 08:54:15 +0900

    ci: refactor npm link script

--- scripts/link-to-example.sh
+++ scripts/link-to-example.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if [ "$1" = "unlink" ]; then
-    cd src || exit 1
-    npm unlink
-    cd ../examples || exit 1
-    npm unlink react-dom-lazyload-component
-    exit 0
-fi
 cd src || exit 1
-npm link
+npm "$1"
 cd ../examples || exit 1
-npm link react-dom-lazyload-component
+npm "$1" react-dom-lazyload-component
