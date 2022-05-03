8995c4873fcd72f3781e162c78a99fe0da1b5853
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-03 11:03:30 +0900

    test: add puppeteer settings

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,10 @@ module.exports = {
   server: {
     command: 'npm run -w examples start',
     port: 3001,
+    host: 'localhost',
+    protocol: 'http',
+    debug: true,
     launchTimeout: 60000,
+    usedPortAction: 'kill',
   },
 };
