d7731da7161e63393dade688ead551c0cf259bde
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-04 15:22:58 +0900

    ci: add uninstall local package script

--- scripts/uninstall-local-package.mjs
+++ scripts/uninstall-local-package.mjs
@@ -1,25 +1,16 @@
 import fs from 'fs/promises';
 import packageJson from '../examples/package.json' assert { type: 'json' };
-import { exec } from './utils.mjs';
 
 const packageName = `react-dom-lazyload-component-${process.env.npm_package_version}.tgz`;
 const baseDir = 'examples';
 const exportDir = `./${baseDir}`;
 
 try {
-  await fs.unlink(packageName);
-} catch (e) {
-  console.debug(e);
-}
-try {
-  await exec('npm run build && npm pack');
-  await fs.copyFile(packageName, `${exportDir}/${packageName}`);
+  await fs.unlink(`./${exportDir}/${packageName}`);
   if (!packageJson.dependencies) {
-    packageJson.dependencies = {};
+    return process.exit(0);
   }
-  packageJson.dependencies[
-    'react-dom-lazyload-component'
-  ] = `file:../${baseDir}/${packageName}`;
+  delete packageJson.dependencies['react-dom-lazyload-component'];
   await fs.unlink(`./${baseDir}/package.json`);
   await fs.writeFile(
     `./${baseDir}/package.json`,
@@ -28,6 +19,4 @@ try {
 } catch (e) {
   console.error(e);
   process.exit(1);
-} finally {
-  await fs.unlink(packageName);
 }
