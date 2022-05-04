13f133d0fde3250e7753910efbeeedee3b11bcff
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-04 09:45:18 +0900

    ci: add install deps only root step

---
+++ scripts/install-deps-only-root.mjs
@@ -0,0 +1,31 @@
+import { promisify } from 'util';
+import { exec as defaultExec } from 'child_process';
+import packageJson from '../package.json' assert { type: 'json' };
+
+const exec = promisify(defaultExec);
+
+const { dependencies, devDependencies } = packageJson;
+
+if (dependencies) {
+  const installDepsCommand = Object.entries(dependencies)
+    .map(entry => `${entry[0]}@${entry[1]}`)
+    .join(' ');
+  try {
+    await exec(`npm i ${installDepsCommand}`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+if (devDependencies) {
+  const installDevDepsCommand = Object.entries(devDependencies)
+    .map(entry => `${entry[0]}@${entry[1]}`)
+    .join(' ');
+  try {
+    await exec(`npm i -D ${installDevDepsCommand}`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
