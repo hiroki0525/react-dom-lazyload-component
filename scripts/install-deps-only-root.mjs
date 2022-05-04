a04aa677f396787dff455d207b84779fe29fe901
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-04 10:40:36 +0900

    ci: fix ci sysytem

--- scripts/install-deps-only-root.mjs
+++ scripts/install-deps-only-root.mjs
@@ -8,10 +8,10 @@ const { dependencies, devDependencies } = packageJson;
 
 if (dependencies) {
   const installDepsCommand = Object.entries(dependencies)
-    .map(entry => `${entry[0]}@${entry[1]} --force`)
+    .map(entry => `${entry[0]}@${entry[1]}`)
     .join(' ');
   try {
-    await exec(`npm i ${installDepsCommand}`);
+    await exec(`npm i ${installDepsCommand} --force`);
   } catch (e) {
     console.error(e);
     process.exit(1);
