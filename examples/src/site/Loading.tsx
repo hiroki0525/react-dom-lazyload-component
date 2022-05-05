d913b115c278eae283f94fff737d95b64e00cd81
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-05 18:31:02 +0900

    chore(examples): change style Loading Component

--- examples/src/site/Loading.tsx
+++ examples/src/site/Loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <img src='/loading.gif' alt='loading' width='48' height='48' />;
+  return (
+    <div className='flex justify-center items-center'>
+      <img src='/loading.gif' alt='loading' width='48' height='48' />
+    </div>
+  );
 }
