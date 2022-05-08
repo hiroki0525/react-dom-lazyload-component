53eecc75acd42963a0271dafe2e86f6a29ec796b
Hiroki Miyaji <hdekm852@gmail.com>
2022-05-08 16:45:05 +0900

    chore(examples): add forceVisible example

---
+++ examples/src/usage/ForceVisibleExample.tsx
@@ -0,0 +1,38 @@
+import LazyLoad from 'react-dom-lazyload-component';
+import { useEffect, useState } from 'react';
+
+export default function ForceVisibleExample() {
+  const [forceVisible, setForceVisible] = useState(false);
+  const timeout = 5000;
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      setForceVisible(true);
+    }, timeout);
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, []);
+
+  return (
+    <section>
+      <h2>forceVisible</h2>
+      <p>Visible after {timeout} ms.</p>
+      <ul id='forceVisible' className='scrollList'>
+        {[...Array(5)].map((_, index) => (
+          <LazyLoad
+            key={`demo3-${index}`}
+            InvisibleComponent='Invisible'
+            rootId='forceVisible'
+            as='li'
+            className='row'
+            rootMargin='-1000px 0px'
+            forceVisible={forceVisible}
+          >
+            Visible
+          </LazyLoad>
+        ))}
+      </ul>
+    </section>
+  );
+}
